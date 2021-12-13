015241764180deeb556d355a490b4d2c0697d916
Heldi Apriadi <61349808+heldiapriadi@users.noreply.github.com>
2021-12-13 23:47:19 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,20 +7,32 @@ import os
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = './static/uploads/'
-model = pickle.load(open('model.pkl','rb'))
+model = load_model('model.h5')
 
+class_dict = {0: 'Cat (Kucing)', 1: 'Dog (Anjing)'}
+
+def predict_label(img_path):
+    loaded_img = load_img(img_path, target_size=(256, 256))
+    img_array = img_to_array(loaded_img) / 255.0
+    img_array = expand_dims(img_array, 0)
+    predicted_bit = np.round(model.predict(img_array)[0][0]).astype('int')
+    return class_dict[predicted_bit]
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        new_review = request.form['komentar']
-        new_review = re.sub('[^a-zA-Z]', ' ', new_review)
-        new_review = new_review.lower()
-        new_review = new_review.split()
-        pred = model.predict(new_review)
-        return render_template('index.html', prediction=pred)
+        if request.files:
+            image = request.files['image']
+            img_path = os.path.join(app.config['UPLOAD_FOLDER'], image.filename)
+            image.save(img_path)
+            prediction = predict_label(img_path)
+            return render_template('index.html', uploaded_image=image.filename, prediction=prediction)
+
     return render_template('index.html')
 
+@app.route('/display/<filename>')
+def send_uploaded_image(filename=''):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 if __name__ == '__main__':
     app.run(debug=True)
